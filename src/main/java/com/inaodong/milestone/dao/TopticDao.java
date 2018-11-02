fa59d373517b01207dd923f047f9c474de45e6f0
HuangXingRui <550406994@qq.com>
2018-11-02 16:06:33 +0800

    tomcat启动报错 mybatisxml语法错误

--- src/main/java/com/inaodong/milestone/dao/TopticDao.java
+++ src/main/java/com/inaodong/milestone/dao/TopticDao.java
@@ -1,21 +1,34 @@
 package com.inaodong.milestone.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.inaodong.milestone.entity.Content;
 import com.inaodong.milestone.entity.Toptic;
 
 public interface TopticDao {
-	
+
 	/**
 	 * 插入toptic放回id
+	 * 
 	 * @param toptic
 	 * @return
 	 */
 	int insertToptic(Toptic toptic);
-	
+
 	/**
 	 * 
 	 * @param content
 	 * @return
 	 */
 	int insertContent(Content content);
+
+	/**
+	 * 返回index页显示的帖子集合
+	 * @param currentPage
+	 * @param pageSize
+	 * @return
+	 */
+	List<Toptic> queryTopticAndContent(@Param("currentPage") Integer currentPage, @Param("pageSize") Integer pageSize);
 }
---
+++ src/main/java/com/inaodong/milestone/dto/PageInfo.java
@@ -0,0 +1,11 @@
+package com.inaodong.milestone.dto;
+
+/**
+ * 返回数据
+ * 用于呈现帖子的信息
+ * @author SEELE
+ *
+ */
+public class PageInfo {
+
+}
